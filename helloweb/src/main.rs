5d2d3476f319239f1f32e538ebf4fb5fda6ee7be
Xiaodong Li <bryne_lxd@sina.com>
2019-09-08 00:53:56 +0800

    added web server demo

--- helloweb/src/main.rs
+++ helloweb/src/main.rs
@@ -1,17 +1,21 @@
-use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::fs;
 use std::thread;
 use std::time::Duration;
+use std::fs;
+use std::io::prelude::*;
+use helloweb::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(2);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
